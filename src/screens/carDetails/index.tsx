3eae6b6b009655d750985937a20f47b51e04db54
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2021-11-10 16:54:11 -0300

    Adicionando mais recursos a interface de detalhes

--- src/screens/carDetails/index.tsx
+++ src/screens/carDetails/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import BackButton from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 
-import { Container, Header, CarImages } from './styles';
+import { Container, Header, CarImages, Content, Details, Description, Brand, Name, Rent, Period, Price, About } from './styles';
 
 export function CarDetails() {
   return (
@@ -10,9 +10,27 @@ export function CarDetails() {
       <Header>
         <BackButton onPress={() => { }} />
       </Header>
+
       <CarImages>
         <ImageSlider imagesUrl={['https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png']} />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$500 </Price>
+          </Rent>
+        </Details>
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+        </About>
+      </Content>
+
     </Container>
   )
 }
--- src/screens/carDetails/styles.ts
+++ src/screens/carDetails/styles.ts
@@ -3,7 +3,7 @@ import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-  flex:1;
+  flex: 1;
   background-color: ${({ theme }) => theme.colors.background.secondary};
 `;
 
@@ -19,4 +19,63 @@ export const Header = styled.View`
 
 export const CarImages = styled.View`
   margin-top: ${getStatusBarHeight() + RFValue(32)}px;
+`;
+
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    padding: 24,
+    alignItems: 'center'
+  },
+  showsVerticalIndicator: false
+})``;
+
+export const Details = styled.View`
+  width: 100%;
+  
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: ${RFValue(38)}px;
+`;
+
+export const Description = styled.View``;
+
+export const Brand = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary.medium_500};
+  color: ${({ theme }) => theme.colors.text_detail};
+  font-size: ${RFValue(10)}px;
+
+  text-transform: uppercase;
+`;
+
+export const Name = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary.medium_500};
+  color: ${({ theme }) => theme.colors.title};
+  font-size: ${RFValue(25)}px;
+`;
+
+export const Rent = styled.View``;
+
+export const Period = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary.medium_500};
+  color: ${({ theme }) => theme.colors.text_detail};
+  font-size: ${RFValue(10)}px;
+  
+  text-transform: uppercase;
+  `;
+
+export const Price = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary.medium_500};
+  color: ${({ theme }) => theme.colors.main};
+  font-size: ${RFValue(25)}px;
+`;
+
+export const About = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary.regular_400};
+  color: ${({ theme }) => theme.colors.text};
+  font-size: ${RFValue(15)}px;
+  text-align: justify;
+  
+  margin-top: ${RFValue(23)}px;
 `;
