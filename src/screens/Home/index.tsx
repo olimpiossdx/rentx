bd00bf439c76a30470f93dbd26edecc21f280923
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2021-11-10 09:26:14 -0300

    Criaando projeto e adicionando primeira tela

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Container, Title } from './styles';
+
+export function Home() {
+  return (
+    <Container>
+      <Title>Home</Title>
+      <Title>Home</Title>
+    </Container>
+  )
+}
