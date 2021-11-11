f12eb8ffed31cffdae514af7faa4b74e37e67e88
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2021-11-11 16:30:45 -0300

    Interface de agendamento

--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import { useTheme } from 'styled-components';
 import BackButton from '../../components/BackButton';
 
-import { Container, Header, Title, DateInfo, RentalPeriod, DateTitle, DateValue } from './styles';
+import { Container, Header, Title, DateInfo, RentalPeriod, DateTitle, DateValue, Content, Footer } from './styles';
 import ArrowSvg from '../../assets/arrow.svg';
+import { StatusBar } from 'react-native';
+import { Button } from '../../components/Button';
+
+interface Props {
+  dateValue: string;
+
+};
 
 export function Scheduling() {
   const theme = useTheme();
   return (
     <Container>
       <Header>
+        <StatusBar barStyle='light-content' translucent backgroundColor='transparent' />
         <BackButton onPress={() => { }} color={theme.colors.shape} />
 
         <Title>
@@ -17,21 +25,26 @@ export function Scheduling() {
           data de início e{'\n'}
           fim do aluguel
         </Title>
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}>10/10/2021</DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}>10/10/2021</DateValue>
+          </DateInfo>
+        </RentalPeriod>
       </Header>
-      <RentalPeriod>
-        <DateInfo>
-          <DateTitle>DE</DateTitle>
-          <DateValue />
-        </DateInfo>
+      <Content>
 
-        <ArrowSvg />
-
-        <DateInfo>
-          <DateTitle>DE</DateTitle>
-          <DateValue />
-        </DateInfo>
-      </RentalPeriod>
-
+      </Content>
+      <Footer>
+        <Button title='Confirmar' />
+      </Footer>
     </Container>
   );
 }
--- src/screens/Scheduling/styles.ts
+++ src/screens/Scheduling/styles.ts
@@ -1,5 +1,6 @@
 import styled, { css } from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 interface DateValueProps {
   selected: boolean;
@@ -17,7 +18,8 @@ export const Header = styled.View`
   background-color: ${({ theme }) => theme.colors.header};
   
   justify-content: center;
-  padding: ${RFValue(24)}px;
+  padding: ${RFValue(25)}px;
+  padding-top: ${getStatusBarHeight() + RFValue(30)}px;
 `;
 
 export const Title = styled.Text`
@@ -28,15 +30,17 @@ export const Title = styled.Text`
   margin-top: ${RFValue(24)}px;
 `;
 
-export const DateInfo = styled.View`
+
+export const RentalPeriod = styled.View`
   width: 100%;
-  
+
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+  margin-top: ${RFValue(32)}px;
 `;
 
-export const RentalPeriod = styled.View`
+export const DateInfo = styled.View`
   width: 30%;
 `;
 
@@ -50,4 +54,22 @@ export const DateValue = styled.Text<DateValueProps>`
   color: ${({ theme }) => theme.colors.shape};
   font-family: ${({ theme }) => theme.fonts.primary.medium_500};
   font-size: ${RFValue(15)}px;
+
+  ${({ selected, theme }) => !selected && css`
+     border-bottom-width: 1px;
+     border-bottom-color: ${theme.colors.text};
+     padding-bottom: ${RFValue(5)}px;
+  `}
+
+`;
+
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    paddingBottom: RFValue(24)
+  },
+  showsVerticalScrollIndicator: false
+})``;
+
+export const Footer = styled.View`
+  padding:${RFValue(24)}px;
 `;
