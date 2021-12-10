67ab1ee4596ba17b110142ab9f795ce7674e906e
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2021-12-10 08:44:32 -0300

    Listando agendamento do usuário

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,8 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container } from './styles'
 
+import { CarDTO } from '../../dtos/CarDTO';
+import api from '../../services/api';
+
 export const MyCars = () => {
-  const [cars, setCars] = useState([]);
+  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    async function fecthCars() {
+      try {
+        const response = await api.get(`/sechedules_byuser?user_id=${1}`);
+        setCars(response.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fecthCars()
+  }, []);
+
   return (
     <Container>
     </Container>
