d965b5c89f6e93b0e649431f157f54714e48e8bf
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2021-12-10 09:13:42 -0300

    Exibindo carros agendados

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from 'react'
-import { Container } from './styles'
+import { FlatList, StatusBar } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
+
+import BackButton from '../../components/BackButton';
 
 import { CarDTO } from '../../dtos/CarDTO';
 import api from '../../services/api';
 
+import { Container, Header, Title, SubTitle, Content, Appointments, AppointmentsTitle, AppointmentsQuantity } from './styles'
+import { Car } from '../../components/Car';
+
+interface CarProps {
+  id: string;
+  user_id: string;
+  car: CarDTO;
+}
+
 export const MyCars = () => {
-  const [cars, setCars] = useState<CarDTO[]>([]);
+  const navigation = useNavigation();
+  const theme = useTheme();
+  const [cars, setCars] = useState<CarProps[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -22,8 +38,46 @@ export const MyCars = () => {
     fecthCars()
   }, []);
 
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
   return (
     <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton onPress={handleBack} color={theme.colors.shape} />
+
+        <Title>
+          Escolha uma {"\n"}
+          data de início e{"\n"}
+          fim do aluguel
+        </Title>
+        <SubTitle>
+          Conforto, segurança e privacidade.
+        </SubTitle>
+      </Header>
+
+      <Content>
+        <Appointments>
+          <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+          <AppointmentsQuantity>05</AppointmentsQuantity>
+        </Appointments>
+        <FlatList
+          data={cars}
+          keyExtractor={item => item.id}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <Car key={item.car.id} data={item.car} />
+          )}
+        />
+
+      </Content>
     </Container>
   )
 }
--- src/screens/MyCars/styles.ts
+++ src/screens/MyCars/styles.ts
@@ -1,3 +1,65 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
+import { RFValue } from "react-native-responsive-fontsize";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: 'center';
+  background-color: ${({ theme }) => theme.colors.background.primary};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFValue(325)}px;
+
+  background-color: ${({ theme }) => theme.colors.header};
+  
+  justify-content: center;
+  padding: ${RFValue(25)}px;
+  padding-top: ${getStatusBarHeight() + RFValue(30)}px;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.secondary.semiBold_600};
+  font-size: ${RFValue(30)}px;
+
+  margin-top: ${RFValue(24)}px;
+`;
+
+export const SubTitle = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.secondary.semiBold_600};
+  font-size: ${RFValue(15)}px;
+
+  margin-top: ${RFValue(24)}px;
+`;
+
+export const Content = styled.View`
+  width: 100%;
+  flex:1;
+  padding: 0 6px;
+`;
+
+export const Appointments = styled.View`
+  width: : 100%;
+
+  flex-direction:row;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 24px 0;
+`;
+
+export const AppointmentsTitle = styled.Text`
+  color: ${({ theme }) => theme.colors.text};
+  font-family: ${({ theme }) => theme.fonts.secondary.regular_400};
+  font-size: ${RFValue(15)}px;
+`;
+
+export const AppointmentsQuantity = styled.Text`
+  color: ${({ theme }) => theme.colors.text};
+  font-family: ${({ theme }) => theme.fonts.secondary.medium_500};
+  font-size: ${RFValue(15)}px;
+`;
